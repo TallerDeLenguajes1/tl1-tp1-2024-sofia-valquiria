77a790e320c226d4068ad4c46383473bbf7ebae5
Sofia Sandoval <sandovalsofi99@gmail.com>
2024-03-30 09:31:30 -0300

    cumpli la consigna e)

--- tp1_1.cpp
+++ tp1_1.cpp
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
 int main(){
-    puts("hola mundo");
+    
+    int numero = 3;
+    int * pNumero;
+    pNumero = &numero;
+    printf("El contenido del puntero es:%d \n", *pNumero);
+    printf("La direccion de memoria almacenada por el puntero es:%p\n", pNumero);
+    printf("La direccion de memoria de la variable numero es: %p\n", &numero);
+    printf("La direccion de memoria del puntero es: %p\n", &pNumero);
+    printf("El tamaño de memoria utilizado por la variable numeroo es: %d\n", sizeof(numero));
     return 0;
 }
